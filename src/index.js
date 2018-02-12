a2a8ae6e67ea12633a7a17215433c0cda2707874
Kuan-Han Chen <kuanhanchen0608@gmail.com>
2018-02-11 17:18:43 -0800

    Implemented video selection by passing callbacks from index to video_list to video_list_item

--- src/index.js
+++ src/index.js
@@ -5,16 +5,23 @@ import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 
-// const API_KEY = 'AIzaSyB51KDqBSAmMwYgg2OviEqfScPV4QjL9AA';
+
 const API_KEY = 'xxxxxxx';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { videos: [] };
+		
+		this.state = { 
+			videos: [],
+			selectVideo: null
+		};
 
 		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-			this.setState({ videos });
+			this.setState({ 
+				videos: videos, 
+				selectedVideo: videos[0]
+			});
 		});
 
 	}
@@ -23,8 +30,8 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
-				<VideoDetail video={this.state.videos[0]} />
-				<VideoList videos={this.state.videos} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos} />
 			</div>	
 		);
 	}
